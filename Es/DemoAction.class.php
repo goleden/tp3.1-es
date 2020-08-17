abb434ceafbcc7892db8ebad77bd7bcd9e48df29
sorry <470134680@qq.com>
2020-08-17 16:50:19 +0800

    demo控制器

--- Es/DemoAction.class.php
+++ Es/DemoAction.class.php
@@ -10,32 +10,38 @@ class DemoAction extends Action
         require __DIR__ . '/../../Es/Curd.php';
 
         echo '<pre>';
+        // 新增、修改
         $client = new Curd([
             'elasticsearch:9200'
         ]);
         $response = $client->save('id' . rand(1, 10), [
             'id' => rand(1, 10),
-            'testField' => '中国人民共和国 中国 中',
-            'testField2' => '中国人民共和国 中国 中'
+            'title' => 'xxx',
+            'title_real' => 'xxx',
+            'content' => 'xxx'
         ]);
         print_r($response);
 
-
-        $results = $client->search('中国', [
+        // 搜索
+        $results = $client->search('test', [
             [
-                'field' => 'testField',
-                'boost' => 3
+                'field' => 'title',
+                'boost' => 10 // 权重
             ],
             [
-                'field' => 'testField2',
+                'field' => 'title_real',
+                'boost' => 10 // 权重
+            ],
+            [
+                'field' => 'content',
                 'boost' => 1
             ],
-        ]);
+        ], 2, 10);
         print_r($results);
 
-
-        $results = $client->delete(1);
-        print_r($results);
+        // 删除
+        $result = $client->delete(1);
+        print_r($result);
 
         exit;
     }
